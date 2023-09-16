debb7918b463d13e8481aa8f0bc4d2fa26400b5a
Silvertide7 <natephillips801@gmail.com>
2023-09-16 13:35:33 -0600

    Add all classes and skills

--- src/main/java/net/silvertide/pmmo_skill_books/utils/SkillBookUtil.java
+++ src/main/java/net/silvertide/pmmo_skill_books/utils/SkillBookUtil.java
@@ -10,9 +10,47 @@ public class SkillBookUtil {
     }
 
     public static String[] getClassList() {
-        return new String[]{"rogue", "fighter"};
+        return new String[]{
+                "artificer",
+                "barbarian",
+                "bard",
+                "cleric",
+                "druid",
+                "fighter",
+                "monk",
+                "paladin",
+                "ranger",
+                "rogue",
+                "sorcerer",
+                "warlock",
+                "wizard",
+        };
     }
     public static String[] getSkillList() {
-        return new String[]{"mining", "swimming"};
+        return new String[] {
+                "magic",
+                "slayer",
+                "fishing",
+                "combat",
+                "alchemy",
+                "mining",
+                "engineering",
+                "endurance",
+                "building",
+                "smithing",
+                "swimming",
+                "woodcutting",
+                "gunslinging",
+                "crafting",
+                "excavation",
+                "farming",
+                "flying",
+                "cooking",
+                "agility",
+                "sailing",
+                "hunter",
+                "archery",
+                "taming"
+        };
     }
 }
