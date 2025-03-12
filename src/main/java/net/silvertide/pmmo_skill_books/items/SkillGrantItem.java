b0d6bad5c62a725325825a3860a64f77f4f0814e
Silvertide7 <natephillips801@gmail.com>
2025-03-12 15:16:09 -0600

    Update item property settings for use in other mod.

--- src/main/java/net/silvertide/pmmo_skill_books/items/SkillGrantItem.java
+++ src/main/java/net/silvertide/pmmo_skill_books/items/SkillGrantItem.java
@@ -7,9 +7,12 @@ import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.*;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
@@ -20,6 +23,7 @@ import net.silvertide.pmmo_skill_books.data.Color;
 import net.silvertide.pmmo_skill_books.data.UseSkillGrantResult;
 import net.silvertide.pmmo_skill_books.utils.*;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
